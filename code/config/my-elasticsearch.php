3904b79924a7cd23ef133a1a9ee9b4353c6270b2
Kiyonori SAKAI <neco.am@gmail.com>
2025-12-07 16:14:49 +0900

    feat: 🔧 config/my-elasticsearch.php Elasticsearch への接続設定の設定ファイルを `php artisan vendor:publish` で出力した

---
+++ code/config/my-elasticsearch.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'hosts' => [
+        'http://elasticsearch:9200',
+    ],
+    'ssl_verification' => false,
+    'user_name'        => null,
+    'password'         => null,
+];
