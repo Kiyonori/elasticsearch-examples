f24f64326488658cc116e1f344c147199d443a65
Kiyonori SAKAI <neco.am@gmail.com>
2025-12-10 05:48:11 +0900

    feat: 🌱 seeders/Demo/DatabaseSeeder デモ用の全体シーダ

--- code/database/seeders/Demo/DatabaseSeeder.php
+++ code/database/seeders/Demo/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             MessageSeeder::class,
+            BreedSeeder::class,
         ]);
     }
 }
