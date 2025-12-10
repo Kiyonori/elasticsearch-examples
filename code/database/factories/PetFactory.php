e47fb57dcf5494134decff0b0b86c85aa47f55f7
Kiyonori SAKAI <neco.am@gmail.com>
2025-12-10 21:23:21 +0900

    feat: 🏭️ factories/PetFactory ペットの名前らしい値を無作為に発行する

--- code/database/factories/PetFactory.php
+++ code/database/factories/PetFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Breed;
 use App\Models\Pet;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PetFactory extends Factory
@@ -12,9 +13,68 @@ class PetFactory extends Factory
 
     public function definition(): array
     {
+        $name = fake()->randomElement([
+            'ウミ',
+            'キナコ',
+            'クロ',
+            'ココ',
+            'コタロウ',
+            'コテツ',
+            'コハク',
+            'サクラ',
+            'ジジ',
+            'シロ',
+            'ソラ',
+            'タマ',
+            'チィ',
+            'チビ',
+            'チャチャ',
+            'チャチャマル',
+            'チョコ',
+            'テン',
+            'トラ',
+            'ナナ',
+            'ハチ',
+            'ハナ',
+            'ハル',
+            'ヒメ',
+            'フク',
+            'ベル',
+            'マル',
+            'マロ',
+            'マロン',
+            'ミー',
+            'ミーチャン',
+            'ミケ',
+            'ミント',
+            'ムギ',
+            'メイ',
+            'モカ',
+            'モモ',
+            'ユズ',
+            'リン',
+            'ルイ',
+            'ルナ',
+            'レオ',
+            'レオン',
+            '花',
+            '空',
+            '小春',
+            '小太郎',
+            '小梅',
+            '大福',
+            '茶々',
+            '茶々丸',
+            '麦',
+            '姫',
+            '福',
+            '凛',
+            '琥珀',
+        ]);
+
         return [
-            'user_id'    => $this->faker->randomNumber(),
-            'name'       => $this->faker->name(),
+            'user_id'    => User::factory(),
+            'name'       => $name,
             'birth_date' => fake()->dateTimeBetween(
                 startDate: today()->subYears(25),
                 endDate: today()->subMonths(6),
