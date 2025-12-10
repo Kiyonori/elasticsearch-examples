917a9709a431ec3d36d51f133bd52f5854b4e216
Kiyonori SAKAI <neco.am@gmail.com>
2025-12-10 21:45:27 +0900

    feat: 🌱 seeders/Demo/DatabaseSeeder デモ用の全体シーダにペットのシーダーを組み込んだ

--- code/database/seeders/Demo/DatabaseSeeder.php
+++ code/database/seeders/Demo/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             MessageSeeder::class,
             BreedSeeder::class,
+            PetSeeder::class,
         ]);
     }
 }
