3fc112fbbeb379e57c68f22e74e373f0adcc4d66
Kiyonori Sakai <neco.am@gmail.com>
2025-12-20 17:16:34 +0900

    feat: 🛣️ Routing. elasticsearch/users

--- code/routes/api.php
+++ code/routes/api.php
@@ -7,7 +7,18 @@ Route::prefix('rdbms')->name('rdbms.')->group(
     function () {
         Route::prefix('users')->name('users.')->group(
             function () {
-                    Route::get('', Users\Rdbms\IndexUsersController::class)
+                Route::get('', Users\Rdbms\IndexUsersController::class)
+                    ->name('index');
+            }
+        );
+    }
+);
+
+Route::prefix('elasticsearch')->name('elasticsearch.')->group(
+    function () {
+        Route::prefix('users')->name('users.')->group(
+            function () {
+                Route::get('', Users\Elasticsearch\IndexUserController::class)
                     ->name('index');
             }
         );
