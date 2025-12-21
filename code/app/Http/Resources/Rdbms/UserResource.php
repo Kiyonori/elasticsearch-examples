fcf65b8a762d6324b25eb29701e7bfa39ac0322b
Kiyonori SAKAI <neco.am@gmail.com>
2025-12-21 14:24:29 +0900

    feat: 🚰 UserResource. 不必要な情報を返さないようにした

--- code/app/Http/Resources/Rdbms/UserResource.php
+++ code/app/Http/Resources/Rdbms/UserResource.php
@@ -14,20 +14,17 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'                => $this->resource->id,
-            'last_name'         => $this->resource->last_name,
-            'last_kana_name'    => $this->resource->last_kana_name,
-            'first_name'        => $this->resource->first_name,
-            'first_kana_name'   => $this->resource->first_kana_name,
-            'email'             => $this->resource->email,
-            'email_verified_at' => $this->resource->email_verified_at,
-            'password'          => $this->resource->password,
-            'remember_token'    => $this->resource->remember_token,
-            'prefecture'        => $this->resource->prefecture,
-            'city'              => $this->resource->city,
-            'street_address'    => $this->resource->street_address,
-            'phone_number'      => $this->resource->phone_number,
-            'memo'              => $this->resource->memo,
+            'id'              => $this->resource->id,
+            'last_name'       => $this->resource->last_name,
+            'last_kana_name'  => $this->resource->last_kana_name,
+            'first_name'      => $this->resource->first_name,
+            'first_kana_name' => $this->resource->first_kana_name,
+            'email'           => $this->resource->email,
+            'prefecture'      => $this->resource->prefecture,
+            'city'            => $this->resource->city,
+            'street_address'  => $this->resource->street_address,
+            'phone_number'    => $this->resource->phone_number,
+            'memo'            => $this->resource->memo,
 
             'pets' => $this->whenLoaded(
                 'pets',
