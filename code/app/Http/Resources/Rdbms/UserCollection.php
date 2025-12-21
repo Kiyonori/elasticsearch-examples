cd5076b5f7ebd4c4dc5a141052bfb279c770f12e
Kiyonori SAKAI <neco.am@gmail.com>
2025-12-22 05:44:26 +0900

    feat: 🚰️ Rdbms/UserCollection. JSON 内に検索に要した時間と、次ページのカーソルを付加する

--- code/app/Http/Resources/Rdbms/UserCollection.php
+++ code/app/Http/Resources/Rdbms/UserCollection.php
@@ -10,17 +10,27 @@ class UserCollection extends ResourceCollection
 {
     /** @var CursorPaginator */
     public $resource;
-    
+
+    public function __construct(
+        $resource,
+        private readonly float $responseTime,
+    ) {
+        parent::__construct($resource);
+    }
+
     public function toArray(Request $request): array
     {
         return [
-            'data' => UserResource::collection(
-                $this->resource->items(),
-            ),
+            'response_time' => $this->responseTime,
+
             'next_cursor' => $this
                 ->resource
                 ->nextCursor()
-                ?->encode()
+                ?->encode(),
+
+            'data' => UserResource::collection(
+                $this->resource->items(),
+            ),
         ];
     }
 }
